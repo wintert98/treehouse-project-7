116adaff14500b0196c0b9f06e2d37e740a0df39
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-02-20 11:27:51 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import apiKey from './config/config.js'
 
+export default class App extends Component {
 
+constructor() {
+  super();
+  this.state = {
+    photos: []
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          
-        </p>
-       
-      </header>
-    </div>
-  );
 }
 
-export default App;
+componentDidMount() {
+  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=sunset&per_page=24&format=json&nojsoncallback=1`)
+    .then(response => {
+      this.setState({
+        photos: response.data.photos
+
+    });
+  })
+    .catch(error => {
+       console.log('Error fetching and parsing data', error);
+    });
+
+}
+
+
+render() {
+  console.log(this.state.photos)
+  return (
+    <div>
+      <div>
+        <div>
+          <h1> </h1>
+        </div>
+      </div>
+      <div>
+      </div>
+    </div>
+
+  );
+
+}
+}
+
+
+
+
